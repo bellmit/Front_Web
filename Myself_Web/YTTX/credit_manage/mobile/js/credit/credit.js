3144370f919e0c71cd5cb9713dc55f3c62ee8638
yipin <yipin8715@sina.com>
2016-05-31 18:35:56 +0800

    add

---
+++ Myself_Web/YTTX/credit_manage/mobile/js/credit/credit.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Administrator on 2016/5/31 0031.
+ */
+ 
+ seajs.use(['jquery','jmb','angular','dialog-config','dialog','popup','drag','artdialog'],function($,$MB,Angular,Dialog_Config,Dialog,Popup,Drap,ArtDialog){
+	 
+	ArtDialog({
+		content:'test artdialog function'
+	}).showModal();
+
+
+
+
+});
