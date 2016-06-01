412fd28ac0b16e482647c1e39e2a8c2934d9b573
yipin <yipin8715@sina.com>
2016-06-01 18:13:33 +0800

    update

--- Myself_Web/YTTX/credit_manage/mobile/js/credit/credit.js
+++ Myself_Web/YTTX/credit_manage/mobile/js/credit/credit.js
@@ -1,4 +1,5 @@
 /**name:credit_manage / credit;
  author:yipin;
  date:2016-06-01;
- version:1.0.0**/var head=angular.module("hearApp",[]);head.controller("headController",function(a){});
+ version:1.0.0**/
+!function(){var a=angular.module("historyApp",[]);a.controller("historyCtrl",["$scope",function(a){}]);var b=angular.module("menuApp",[]);b.controller("menuCtrl",["$scope",function(a){var b=[{href:"../community/community.html","class":"menu-sq"},{href:"credit.html","class":"menu-xyk"},{href:"../community/share_community.html","class":"menu-fw"},{href:"#","class":"menu-sc"},{href:"#","class":"menu-wo"}];a.menuitem=b}]),angular.bootstrap(document.getElementById("app_menu"),["menuApp"])}();
--- Myself_Web/YTTX/credit_manage/mobile/src/credit/credit.js
+++ Myself_Web/YTTX/credit_manage/mobile/src/credit/credit.js
@@ -2,10 +2,46 @@
  * Created by Administrator on 2016/5/31 0031.
  */
 /*程序主入口*/
-var head=angular.module('hearApp',[]);
-
-head.controller('headController',function($scope){
+(function () {
 
 
+    //历史服务模块
+    var historyapp=angular.module('historyApp',[]);
 
-});
+    historyapp.controller("historyCtrl",['$scope',function($scope){
+
+    }]);
+
+
+
+    //菜单服务模块
+    var menuapp=angular.module("menuApp",[]);
+
+    menuapp.controller("menuCtrl",['$scope',function($scope){
+        var menuitem=[{
+            "href":"../community/community.html",
+            "class":"menu-sq"
+        },{
+            "href":"credit.html",
+            "class":"menu-xyk"
+        },{
+            "href":"../community/share_community.html",
+            "class":"menu-fw"
+        },{
+            "href":"#",
+            "class":"menu-sc"
+        },{
+            "href":"#",
+            "class":"menu-wo"
+        }];
+        $scope.menuitem=menuitem;
+    }]);
+
+
+    //手动启动任务
+    //angular.bootstrap(angular.element("#app_history"),["historyApp"]);
+    angular.bootstrap(document.getElementById("app_menu"),["menuApp"]);
+
+
+}());
+
