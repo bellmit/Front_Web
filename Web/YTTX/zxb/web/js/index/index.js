eec1f97688d9b42e28e07080e5951c5d6caf0670
yipin8715@sina.com <yipin8715@sina.com>
2016-07-24 13:10:14 +0800

    add or update

--- Web/YTTX/zxb/web/js/index/index.js
+++ Web/YTTX/zxb/web/js/index/index.js
@@ -1,16 +1,17 @@
 (function($){
 	"use strict";
 	$(function(){
-
+		/*dom引用*/
+		var $screen_btn=$('#screen_btn'),
+			$screen1=$('#screen1'),
+			$screen2=$('#screen2'),
+			$screen3=$('#screen3'),
+			$screen4=$('#screen4'),
+			$screen5=$('#screen5');
 		
 
 		
 		
 	});
-	
-	
 
-	
-	
-	
 })(jQuery);
