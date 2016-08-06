1b93b3e97ed380b0de51bc1fb214cd24f4137c50
yipin8715@sina.com <yipin8715@sina.com>
2016-08-06 17:57:57 +0800

    update

--- Web/YTTX/credit_admin/web/js/admin/admin_role.js
+++ Web/YTTX/credit_admin/web/js/admin/admin_role.js
@@ -24,13 +24,20 @@
 			var powermap=public_tool.getPower();
 
 			/*dom引用和相关变量定义*/
-			var $admin_role_wrap=$('#admin_role_wrap')/*表格*/,
+			var $admin_role_wrap=$('#admin_role_wrap')/*角色表格*/,
+				$admin_member_wrap=$('#admin_member_wrap')/*成员表格*/,
 				module_id='admin_role'/*模块id，主要用于本地存储传值*/,
-				table=null/*datatable 解析后的对象*/,
+				table=null/*datatable 角色解析后的对象*/,
+				table_member=null/*datatable 成员解析后的对象*/,
+				$member_wrap=$('#member_wrap')/*成员操作区域*/,
 				$table_wrap=$('#table_wrap')/*表格容器*/,
-				$edit_wrap=$('#edit_wrap')/*编辑容器*/,
+				$table_member_wrap=$('#table_member_wrap')/*表格容器*/,
+				$edit_wrap=$('#edit_wrap')/*角色编辑容器*/,
+				$edit_member_wrap=$('#edit_member_wrap')/*成员编辑容器*/,
 				$role_add_btn=$('#role_add_btn'),/*添加角色*/
-				$edit_close_btn=$('#edit_close_btn')/*编辑关闭按钮*/,
+				$member_add_btn=$('#member_add_btn'),/*添加成员*/
+				$edit_close_btn=$('#edit_close_btn')/*角色编辑关闭按钮*/,
+				$editmember_close_btn=$('#editmember_close_btn')/*成员编辑关闭按钮*/,
 				dia=dialog({
 					title:'温馨提示',
 					okValue:'确定',
@@ -49,21 +56,49 @@
 				$role_edit_form=$('#role_edit_form')/*编辑表单*/,
 				$role_id=$('#role_id'),/*角色id*/
 				$role_name=$('#role_name'),/*角色名称*/
-				$role_remark=$('#role_remark')/*角色描述*/;
+				$role_remark=$('#role_remark'),/*角色描述*/
+				$editmember_cance_btn=$('#editmember_cance_btn')/*编辑取消按钮*/,
+				editmember_form=document.getElementById('member_edit_form'),
+				$member_edit_form=$('#member_edit_form')/*编辑表单*/,
+				$member_id=$('#member_id'),/*成员id*/
+				$member_username=$('#member_username'),/*成员登陆名*/
+				$member_password=$('#member_password'),/*成员密码*/
+				$member_name=$('#member_name')/*成员名称*/;
+
+
+			/*成员请求信息*/
+			var member_config={
+						url:"http://120.24.226.70:8081/yttx-adminbms-api/sysusers",
+						dataType:'JSON',
+						method:'post',
+						dataSrc:function ( json ) {
+							var code=parseInt(json.code,10);
+							if(code!==0){
+								console.log(json.message);
+								return null;
+							}
+							return json.result.list;
+						},
+						data:{
+							roleId:'-1',
+							adminId:decodeURIComponent(logininfo.param.adminId),
+							token:decodeURIComponent(logininfo.param.token)
+						}
+				};
 
 
 
-			/*数据加载*/
+			/*角色数据加载*/
 			table=$admin_role_wrap.DataTable({
 				deferRender:true,/*是否延迟加载数据*/
 				//serverSide:true,/*是否服务端处理*/
-				searching:false,/*是否搜索*/
+				searching:true,/*是否搜索*/
 				ordering:false,/*是否排序*/
 				//order:[[1,'asc']],/*默认排序*/
-				paging:false,/*是否开启本地分页*/
+				paging:true,/*是否开启本地分页*/
 				pagingType:'simple_numbers',/*分页按钮排列*/
 				autoWidth:true,/*是否*/
-				info:false,/*显示分页信息*/
+				info:true,/*显示分页信息*/
 				stateSave:false,/*是否保存重新加载的状态*/
 				processing:true,/*大消耗操作时是否显示处理状态*/
 				ajax:{
@@ -90,7 +125,7 @@
 						public_tool.removeParams(module_id);
 						if(param){
 							return {
-								roleId:decodeURIComponent(logininfo.param.roleId),
+								roleId:param.roleId,
 								adminId:decodeURIComponent(logininfo.param.adminId),
 								token:decodeURIComponent(logininfo.param.token)
 							};
@@ -106,7 +141,7 @@
 					{
 						"data":"id",
 						"render":function(data, type, full, meta ){
-							return '<input type="checkbox" data-id="'+full.id+'" name="role" class="cbr">';
+							return '<input type="checkbox" data-id="'+data+'" name="role" class="cbr">';
 						}
 					},
 					{"data":"name"},
@@ -114,53 +149,115 @@
 					{
 						"data":"id",
 						"render":function(data, type, full, meta ){
-							var id=parseInt(full.id,10),
+							var id=parseInt(data,10),
 								btns='';
 
-							if(id===1){
-								//超级管理员
-								if(typeof powermap[3]!=='undefined'){
-									btns='<span data-id="'+id+'" data-action="update" class="btn btn-white btn-icon btn-xs g-br2 g-c-gray8">\
+							/*修改*/
+							if(typeof powermap[3]!=='undefined'){
+								btns+='<span data-id="'+id+'" data-action="update" class="btn btn-white btn-icon btn-xs g-br2 g-c-gray8">\
 									 <i class="fa-pencil"></i>\
 									 <span>修改</span>\
 									 </span>';
-								}
-							}else{
-								//管理员||高级会员
-								/*修改*/
-								if(typeof powermap[3]!=='undefined'){
-									btns+='<span data-id="'+id+'" data-action="update" class="btn btn-white btn-icon btn-xs g-br2 g-c-gray8">\
-									 <i class="fa-pencil"></i>\
-									 <span>修改</span>\
+							}
+							/*成员*/
+							if(typeof powermap[7]!=='undefined'){
+								btns+='<span data-action="select" data-id="'+id+'" class="btn btn-white btn-icon btn-xs g-br2 g-c-gray8">\
+									 <i class="fa-group"></i>\
+									 <span>成员</span>\
 									 </span>';
-								}
-								/*成员*/
-								btns+='<span data-href="admin_member.html" data-module="admin_member" data-action="select" data-id="'+id+'" class="btn btn-white btn-icon btn-xs g-br2 g-c-gray8">\
-								 <i class="fa-group"></i>\
-								 <span>成员</span>\
-								 </span>';
-								/*权限*/
-								if(typeof powermap[6]!=='undefined'){
-									btns+='<span data-href="yttx-admin-permission.html" data-module="admin_permission" data-action="select" data-id="'+id+'"  class="btn btn-white btn-icon btn-xs g-br2 g-c-gray8">\
+							}
+
+							/*权限*/
+							if(typeof powermap[6]!=='undefined'){
+								btns+='<span data-href="yttx-admin-permission.html" data-module="admin_permission" data-action="select" data-id="'+id+'"  class="btn btn-white btn-icon btn-xs g-br2 g-c-gray8">\
 									<i class="fa-gear"></i>\
 									<span>权限</span>\
 									</span>';
-								}
-								/*删除*/
-								if(typeof powermap[4]!=='undefined'){
+							}
+							/*删除*/
+							if(typeof powermap[4]!=='undefined'){
+								if(id!==1){
 									btns+='<span  data-id="'+id+'" data-action="delete" class="btn btn-white btn-icon btn-xs g-br2 g-c-gray8">\
 									<i class="fa-trash"></i>\
 									<span>删除</span>\
 									</span>';
 								}
 							}
+
 							return btns;
 						}
 					}
 				],/*控制分页数*/
-				lengthChange:false/*是否可改变长度*/
+				aLengthMenu: [
+					[5,10,15],
+					[5,10,15]
+				],
+				lengthChange:true/*是否可改变长度*/
 			});
 
+			/*成员数据加载*/
+			table_member=$admin_member_wrap.DataTable({
+				deferRender:true,/*是否延迟加载数据*/
+				//serverSide:true,/*是否服务端处理*/
+				searching:true,/*是否搜索*/
+				ordering:false,/*是否排序*/
+				//order:[[1,'asc']],/*默认排序*/
+				columnDefs:[],/*配置列状态*/
+				paging:true,/*是否开启本地分页*/
+				pagingType:'simple_numbers',/*分页按钮排列*/
+				autoWidth:true,/*是否*/
+				info:true,/*显示分页信息*/
+				stateSave:false,/*是否保存重新加载的状态*/
+				processing:true,/*大消耗操作时是否显示处理状态*/
+				ajax:member_config,/*异步请求地址及相关配置*/
+				columns: [
+					{
+						"data":"id",
+						"render":function(data, type, full, meta ){
+							return '<input type="checkbox" data-id="'+data+'" name="role" class="cbr">';
+						}
+					},
+					{"data":"username"},
+					{"data":"name"},
+					{
+						"data":"id",
+						"render":function(data, type, full, meta ){
+							var id=parseInt(data,10),
+								btns='';
+
+							/*修改*/
+							if(typeof powermap[8]!=='undefined'){
+								btns+='<span data-id="'+id+'" data-action="update" class="btn btn-white btn-icon btn-xs g-br2 g-c-gray8">\
+									 <i class="fa-pencil"></i>\
+									 <span>修改</span>\
+									 </span>';
+							}
+
+
+							/*删除*/
+							if(typeof powermap[9]!=='undefined'){
+									btns+='<span  data-id="'+id+'" data-action="delete" class="btn btn-white btn-icon btn-xs g-br2 g-c-gray8">\
+									<i class="fa-trash"></i>\
+									<span>删除</span>\
+									</span>';
+							}
+							return btns;
+						}
+					},
+					{
+						"data":"password",
+						"className":'g-d-hidei',
+						"render":function(data, type, full, meta ){
+							return encodeURIComponent(data);
+						}
+					}
+				],/*控制分页数*/
+				aLengthMenu: [
+					[5,10,15],
+					[5,10,15]
+				],
+				lengthChange:true/*是否可改变长度*/
+			});
 
 
 
@@ -246,16 +343,7 @@
 									},100);
 								})
 								.fail(function(resp){
-									if(!resp.flag&&resp.message){
-										setTimeout(function(){
-											self.content('<span class="g-c-bs-warning g-btips-warn">'+resp.message+'</span>');
-										},100);
-									}else{
-										setTimeout(function(){
-											self.content('<span class="g-c-bs-warning g-btips-warn">删除数据失败</span>');
-										},100);
-
-									}
+									console.log(resp.message);
 								});
 						},'admin_delete');
 						//确认删除
@@ -284,6 +372,11 @@
 							}
 						}
 
+					}else if(action==='select'){
+						/*查询操作*/
+						$member_wrap.attr({'data-id':id});
+						member_config.data.roleId=id;
+						table_member.ajax.config(member_config).load();
 					}
 				}
 
@@ -293,43 +386,83 @@
 
 
 			/*//取消修改*/
-			$edit_cance_btn.on('click',function(e){
+			$.each([$edit_cance_btn,$editmember_cance_btn],function () {
+				var selector=this.selector;
 				//切换显示隐藏表格和编辑区
-				$table_wrap.removeClass('col-md-9');
-				$edit_wrap.removeClass('g-d-showi');
+				this.on('click',function(e){
+					if(selector.indexOf('member')!==-1){
+						//成员
+						$table_member_wrap.removeClass('col-md-9');
+						$edit_member_wrap.removeClass('g-d-showi');
+					}else{
+						//角色
+						$table_wrap.removeClass('col-md-9');
+						$edit_wrap.removeClass('g-d-showi');
+					}
+				});
 			});
 
-			/*添加角色*/
-			if(typeof powermap[2]!=='undefined'){
-				$role_add_btn.on('click',function(){
-					//重置表单
-					edit_form.reset();
-					$edit_close_btn.prev().html('添加角色');
-					$edit_cance_btn.prev().html('添加角色');
-					//显示表单
-					$table_wrap.addClass('col-md-9');
-					$edit_wrap.addClass('g-d-showi');
-					//第一行获取焦点
-					$role_name.focus();
+
+			/*添加角色,成员*/
+			$.each([$role_add_btn,$member_add_btn],function(){
+				var selector=this.selector;
+
+				/*绑定事件*/
+				this.on('click',function(){
+					if(selector.indexOf('role')!==-1){
+						//角色
+						edit_form.reset();
+						$edit_close_btn.prev().html('添加角色');
+						$edit_cance_btn.prev().html('添加角色');
+						//显示表单
+						$table_wrap.addClass('col-md-9');
+						$edit_wrap.addClass('g-d-showi');
+						//第一行获取焦点
+						$role_name.focus();
+					}else{
+						//成员
+						editmember_form.reset();
+						$editmember_close_btn.prev().html('添加成员');
+						$editmember_cance_btn.prev().html('添加成员');
+						//显示表单
+						$table_member_wrap.addClass('col-md-9');
+						$edit_member_wrap.addClass('g-d-showi');
+						//第一行获取焦点
+						$member_username.focus();
+					}
 				});
-			}else{
-				$role_add_btn.addClass('g-d-hidei');
-			}
+
+				/*显示*/
+				if(selector.indexOf('role')!==-1&&typeof powermap[2]!=='undefined'){
+					this.removeClass('g-d-hidei');
+				}else if(selector.indexOf('member')!==-1&&typeof powermap[7]!=='undefined'){
+					this.removeClass('g-d-hidei');
+				}
+			})
 
 
 			/*//关闭编辑区*/
-			$edit_close_btn.click(function(e){
-				e.preventDefault();
-				$edit_cance_btn.trigger('click');
+			$.each([$edit_close_btn,$editmember_close_btn],function(){
+				var selector=this.selector;
+				this.on('click',function(e){
+					e.preventDefault();
+					if(selector.indexOf('member')!==-1){
+						$editmember_cance_btn.trigger('click');
+					}else{
+						$edit_cance_btn.trigger('click');
+					}
+				});
+
 			});
 
 
 			/*表单验证*/
 			if($.isFunction($.fn.validate)) {
 				/*配置信息*/
-				var form_opt={};
+				var form_opt_0={},
+						form_opt_1={};
 				if(public_tool.cache.form_opt_0){
-					$.extend(true,form_opt,public_tool.cache.form_opt_0,{
+					$.extend(true,form_opt_0,public_tool.cache.form_opt_0,{
 						submitHandler: function(form){
 							//判断是否存在id号
 							var id=$role_id.val();
@@ -348,7 +481,6 @@
 												"description":$role_remark.val()
 											}
 										};
-								//config.data['id']=id;
 							}else{
 								//添加角色
 								var config={
@@ -405,13 +537,98 @@
 							return false;
 						}
 					});
+
+					/*提交验证*/
+					$role_edit_form.validate(form_opt_0);
 				}
-				/*提交验证*/
-				$role_edit_form.validate(form_opt);
+
+				if(public_tool.cache.form_opt_1){
+					$.extend(true,form_opt_1,public_tool.cache.form_opt_1,{
+						submitHandler: function(form){
+							//判断是否存在id号
+							var id=$member_id.val();
+							if(id!==''){
+								//修改成员
+								var config={
+									url:"http://120.24.226.70:8081/yttx-adminbms-api/sysuser/update",
+									method: 'POST',
+									dataType: 'json',
+									data:{
+										"roleId":id,
+										"adminId":decodeURIComponent(logininfo.param.adminId),
+										"token":decodeURIComponent(logininfo.param.token),
+										"updateUserId":decodeURIComponent(logininfo.param.adminId),
+										"username":$role_name.val(),
+										"description":$role_remark.val()
+									}
+								};
+							}else{
+								//添加成员
+								var config={
+									url:"http://120.24.226.70:8081/yttx-adminbms-api/sysuser/add",
+									method: 'POST',
+									dataType: 'json',
+									data:{
+										"roleId":$member_wrap.attr('data-id'),
+										"adminId":decodeURIComponent(logininfo.param.adminId),
+										"token":decodeURIComponent(logininfo.param.token),
+										"addUserId":decodeURIComponent(logininfo.param.adminId),
+										"username":$member_username.val(),
+										"password":$member_password.val(),
+										"name":$member_name.val()
+									}
+								};
+							}
+
+							$.ajax(config)
+								.done(function(resp){
+									var code=parseInt(resp.code,10);
+									if(code!==0){
+										console.log(resp.message);
+										dia.content('<span class="g-c-bs-warning g-btips-warn">操作失败</span>').show();
+										setTimeout(function () {
+											dia.close();
+										},2000);
+										return false;
+									}
+
+									if(id!==''){
+										//修改
+										dia.content('<span class="g-c-bs-success g-btips-succ">修改成功</span>').show();
+
+									}else{
+										//添加
+										dia.content('<span class="g-c-bs-success g-btips-succ">添加成功</span>').show();
+									}
+									//重置表单
+									$edit_cance_btn.trigger('click');
+									//重绘表格
+									table.ajax.reload();
+									setTimeout(function () {
+										dia.close();
+									},2000);
+								})
+								.fail(function(){
+									dia.content('<span class="g-c-bs-warning g-btips-warn">操作失败</span>').show();
+									setTimeout(function () {
+										dia.close();
+									},2000);
+
+								});
+
+							return false;
+						}
+					});
+
+
+					/*提交验证*/
+					$role_edit_form.validate(form_opt_1);
+				}
 			}
 
-		}
 
+
+		}
 
 
 
