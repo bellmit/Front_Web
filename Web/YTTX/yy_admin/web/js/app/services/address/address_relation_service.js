4c63b99230625cfa01e72aca576fc3432ae8236d
yipin8715@sina.com <yipin8715@sina.com>
2017-05-11 09:04:48 +0800

    update

--- Web/YTTX/yy_admin/web/js/app/services/address/address_relation_service.js
+++ Web/YTTX/yy_admin/web/js/app/services/address/address_relation_service.js
@@ -8,6 +8,8 @@ angular.module('app')
 
 		/*初始化配置*/
 		this.addressRelation=function (type,model) {
+
+
 			toolUtil
 				.requestHttp({
 					url:BASE_CONFIG.commondomain+BASE_CONFIG.commonproject+'/organization/lowers/search',
@@ -137,56 +139,6 @@ angular.module('app')
 							console.log('请求菜单失败');
 						}
 					});
-		};
-		
-
-		/*初始化组件*/
-		this.initWidget=function (tablecolumn,tablecache) {
-			/*隐藏*/
-			var tempid,
-				str='',
-				i=0;
-
-
-			for(i;i<tablecolumn.hide_len;i++){
-				tempid=tablecolumn.hide_list[i];
-				str+='<li data-value="'+tempid+'">第'+(tempid + 1)+'列</li>';
-				tablecache.column(tempid).visible(false);
-			}
-			if(str!==''){
-				/*赋值控制下拉选项*/
-				$(str).appendTo(tablecolumn.$column_ul.html(''));
-			}
-			/*设置分组*/
-			tablecolumn.$colgroup.html(self.createColgroup(tablecolumn,tablecolumn.hide_len));
-		};
-		
-		/*绑定相关事件*/
-		this.bind=function (tablecolumn,tablecache) {
-			/*绑定切换列控制按钮*/
-			tablecolumn.$column_btn.on('click',function () {
-				tablecolumn.$column_wrap.toggleClass('g-d-hidei');
-			});
-			/*绑定操作列数据*/
-			tablecolumn.$column_ul.on('click','li',function () {
-				/*切换显示相关列*/
-				var $this=$(this),
-					active=$this.hasClass('action-list-active'),
-					index=$this.attr('data-value');
-
-				if(active){
-					$this.removeClass('action-list-active');
-					tablecache.column(index).visible(false);
-				}else{
-					$this.addClass('action-list-active');
-					tablecache.column(index).visible(true);
-				}
-
-				var count=tablecolumn.$column_ul.find('.action-list-active').size();
-
-				/*设置分组*/
-				tablecolumn.$colgroup.html(self.createColgroup(tablecolumn,tablecolumn.hide_len - count));
-			});
 		};
 
 	}]);
