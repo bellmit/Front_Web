2b6cadfcbf231fb42f5178934a3f751eef51bfe5
yipin8715@sina.com <yipin8715@sina.com>
2017-06-01 18:08:40 +0800

    add or update

--- Web/YTTX/gj/web/js/app/index.js
+++ Web/YTTX/gj/web/js/app/index.js
@@ -5,7 +5,25 @@
     'use strict';
 
     $(function () {
-        /**/
-        console.log('test');
+        //轮播dom节点
+        var $slideimg_show=$('#slideimg_show'),
+            $slide_tips=$('#slide_tips'),
+            $slide_img=$('#slide_img'),
+            $slideimg_btn=$('#slideimg_btn');
+
+
+        //轮播动画
+        slide.slideToggle({
+            $wrap:$slideimg_show,
+            $slide_img:$slide_img,
+            $btnwrap:$slideimg_btn,
+            $slide_tipwrap:$slide_tips,
+            minwidth:1000,
+            isresize:false,
+            size:3,
+            times:5000,
+            eff_time:500,
+            btn_active:'slidebtn-active'
+        });
     });
 })(jQuery);
