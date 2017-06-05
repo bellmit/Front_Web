ea3cc2dc742a7b61c1997975f82ae5a9e5aa48d0
yipin8715@sina.com <yipin8715@sina.com>
2017-06-05 17:03:37 +0800

    add or update

---
+++ Web/YTTX/yy_admin/yy_admin_修改头部菜单备份/js/app/module/index/index_controller.js
@@ -0,0 +1,21 @@
+angular.module('app')
+    .controller('IndexController', ['loginService',function(loginService){
+
+
+
+    this.quickitem=[];
+
+    /*主内容快捷导航*/
+    loginService.getMenuData(this.quickitem,false);
+
+            
+    /*主内容侧边栏*/
+    this.menuitem=Mock.mock({
+        'list|2-10':[{
+            "name":/[a-z]{2,5}/,
+            "value":/[0-9a-zA-Z]{2,10}/
+        }]
+    }).list;
+
+
+}]);
