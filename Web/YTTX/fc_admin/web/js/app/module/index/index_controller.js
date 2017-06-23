469c5d5b129d26d762c1f0b76e1a9b6e0ba1ccba
yipin8715@sina.com <yipin8715@sina.com>
2017-06-23 17:42:17 +0800

    add or update

--- Web/YTTX/fc_admin/web/js/app/module/index/index_controller.js
+++ Web/YTTX/fc_admin/web/js/app/module/index/index_controller.js
@@ -11,7 +11,7 @@ angular.module('app')
                 "value": /[0-9a-zA-Z]{2,10}/
             }]
         }).list;
-        
+
         /*处理图像路径*/
         function doQuickImage(arr) {
             var len = arr.length,
@@ -22,16 +22,17 @@ angular.module('app')
                     'finance': 3/*财务*/,
                     'equipment': 4/*设备*/,
                     'setting': 5/*设置*/,
-                    'invoice':6/*发货*/,
-                    'purchase':7/*采购*/,
-                    'warehouse':8/*仓库*/
+                    'invoice': 6/*发货*/,
+                    'purchase': 7/*采购*/,
+                    'warehouse': 8/*仓库*/,
+                    'equity': 9/*股权投资人*/
                 },
                 i = 0,
                 item;
             if (len !== 0) {
                 for (i; i < len; i++) {
-                    item=arr[i];
-                    item['src']='quick_'+imgsrc_map[item['href']]+'.png';
+                    item = arr[i];
+                    item['src'] = 'quick_' + imgsrc_map[item['href']] + '.png';
                 }
             }
             return arr;
