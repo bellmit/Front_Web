a9e8c912118a237301f4bad9737df953a285b63e
yipin8715@sina.com <yipin8715@sina.com>
2017-08-23 19:37:54 +0800

    add or update

--- Web/YTTX/bzw_admin/web/js/app/config.js
+++ Web/YTTX/bzw_admin/web/js/app/config.js
@@ -1,26 +1,42 @@
 /*应用程序初始化配置*/
-var system_app = angular.module('app')
+'use strict';
+/*定义常量*/
+angular
+    .module('app')
     .constant('BASE_CONFIG', {
-        unique_key: 'fc_admin_unique_key'/*系统缓存key键*/,
-        basedomain: 'http://10.0.5.226:8883'/*请求域名*/
+        unique_key: 'bzw_admin_unique_key'/*系统缓存key键*/,
+        basedomain: 'http://10.0.5.226:8082'/*请求域名*/
         /*
          test:http://10.0.5.226:8883
          debug:http://10.0.5.222:8080
          */,
-        debug: false/*调试模式开关*/,
-        baseproject: '/bms-bzwfc-api'/*请求工程地址*/,
-        submenulimit: 6/*系统允许递归嵌套菜单最大层级*/,
+        baseproject: '/mall-buzhubms-api'/*请求工程地址*/,
         commondomain: 'http://120.76.237.100:8080',
         commonproject: '/yttx-public-api'
-    })
-    .config(['$controllerProvider', '$compileProvider', '$filterProvider', '$provide',
-        function ($controllerProvider, $compileProvider, $filterProvider, $provide) {
-            system_app.controller = $controllerProvider.register;
-            system_app.directive = $compileProvider.directive;
-            system_app.filter = $filterProvider.register;
-            system_app.factory = $provide.factory;
-            system_app.service = $provide.service;
-            system_app.constant = $provide.constant;
-            system_app.value = $provide.value;
-        }
-    ]);
+    });
+
+
+
+/*配置*/
+/*
+(function () {
+    'use strict';
+    /!*使用模块配置*!/
+    var system_app = angular
+        .module('app')
+        .config('initConfig', initConfig);
+
+    /!*模块依赖注入*!/
+    initConfig.$inject = ['$controllerProvider', '$compileProvider', '$filterProvider', '$provide'];
+
+    /!*模块实现*!/
+    function initConfig($controllerProvider, $compileProvider, $filterProvider, $provide) {
+        system_app.controller = $controllerProvider.register;
+        system_app.directive = $compileProvider.directive;
+        system_app.filter = $filterProvider.register;
+        system_app.factory = $provide.factory;
+        system_app.service = $provide.service;
+        system_app.constant = $provide.constant;
+        system_app.value = $provide.value;
+    }
+}());*/
