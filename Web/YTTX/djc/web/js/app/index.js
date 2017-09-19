8d7c8e9ff27c5221e0cec57547aa8ac31e1c6904
yipin8715@sina.com <yipin8715@sina.com>
2017-09-19 19:16:25 +0800

    add or update

--- Web/YTTX/djc/web/js/app/index.js
+++ Web/YTTX/djc/web/js/app/index.js
@@ -3,111 +3,176 @@
  */
 (function ($) {
     'use strict';
-
     $(function () {
         /*dom节点缓存*/
-        var $notice_list_wrap = $('#notice_list_wrap');
+        var mini_screen_height = 900,
+            $menu_toggle = ('#menu_toggle'),
+            $menu_wrap = $('#menu_wrap'),
+            $menu_item = $menu_wrap.children(),
+            $index_view = $('#index_view'),
+            $about_view = $('#about_view'),
+            $introduction_view = $('#introduction_view'),
+            $description_view = $('#description_view'),
+            $contact_view = $('#contact_view'),
+            $win = $(window),
+            screen_pos = [
+                {
+                    node: $index_view,
+                    pos: 0
+                },
+                {
+                    node: $about_view,
+                    pos: 0
+                },
+                {
+                    node: $introduction_view,
+                    pos: 0
+                },
+                {
+                    node: $description_view,
+                    pos: 0
+                },
+                {
+                    node: $contact_view,
+                    pos: 0
+                }
+            ],
+            isMobile = false;
 
 
-        //轮播dom节点
-        var $slideimg_show = $('#slideimg_show'),
-            $slide_tips = $('#slide_tips'),
-            $slide_img = $('#slide_img'),
-            $slideimg_btn = $('#slideimg_btn');
+        /*初始化*/
+        (function () {
+            var i = 0,
+                len = screen_pos.length,
+                j = 0,
+                pos = $(window).scrollTop();
+
+            /*初始化屏幕*/
+            for (i; i < len; i++) {
+                var temptop = screen_pos[i]["node"].offset().top;
+                screen_pos[i]["pos"] = temptop;
+
+                var minpos = parseInt(pos - 150, 0),
+                    maxpos = parseInt(pos + 150, 0);
+                if (temptop >= minpos && temptop <= maxpos) {
+                    $menu_item.eq(i).addClass('menu-active').siblings().removeClass('menu-active');
+                }
+            }
+
+            /*初始化视口判断*/
+            var winwidth = $win.width();
+            if (winwidth >= 1200) {
+                isMobile = false;
+            } else {
+                isMobile = true;
+            }
+
+        }());
 
 
-        //轮播动画
-        slide.slideToggle({
-            $wrap: $slideimg_show,
-            $slide_img: $slide_img,
-            $btnwrap: $slideimg_btn,
-            $slide_tipwrap: $slide_tips,
-            minwidth: 1000,
-            isresize: false,
-            size: 3,
-            times: 5000,
-            eff_time: 500,
-            btn_active: 'slidebtn-active'
+        //监听菜单导航
+        $menu_wrap.on('click', function (e) {
+            e.stopPropagation();
+            //e.preventDefault();
+            var target = e.target,
+                node = target.nodeName.toLowerCase(),
+                $li;
+            if (node === 'a' || node === 'span') {
+                $li = $(target).closest('li');
+            } else if (node === 'li') {
+                $li = $(target);
+            } else {
+                return false;
+            }
+
+
+            var index = $li.index();
+            if (isMobile) {
+                $('html,body').animate({'scrollTop': screen_pos[index]['pos'] - 40 + 'px'}, 500);
+            } else {
+                $('html,body').animate({'scrollTop': screen_pos[index]['pos'] - 100 + 'px'}, 500);
+            }
         });
 
 
-        /*初始化加载公告数据*/
-        getNoticeList({
-            $list: $notice_list_wrap
-        });
+        //监听导航切换显示隐藏
+        /*$menu_toggle.on('click',function(){
+         if($header_btn.hasClass('header-btnactive')){
+         //隐藏
+         $header_btn.removeClass('header-btnactive');
+         $header_menu.removeClass('g-d-showi');
+         }else{
+         //显示
+         $header_btn.addClass('header-btnactive');
+         $header_menu.addClass('g-d-showi');
+         }
+         });*/
+
+
+        //监听菜单滚动条滚动
+        (function () {
+            var count = 0;
+            $win.on('scroll resize', function (e) {
+                var type = e.type;
+                if (type == 'scroll') {
+                    (function () {
+                        count++;
+                        if (count % 2 == 0) {
+                            var $this = $(this),
+                                currenttop = $this.scrollTop(),
+                                i = 0,
+                                len = screen_pos.length;
+
+                            for (i; i < len; i++) {
+                                var pos = screen_pos[i]['pos'],
+                                    minpos = parseInt(pos - 150, 0),
+                                    maxpos = parseInt(pos + 150, 0);
+
+                                if (currenttop >= minpos && currenttop <= maxpos) {
+                                    $menu_item.eq(i).addClass('menu-active').siblings().removeClass('menu-active');
+                                }
+                            }
+
+                        }
+                    }());
+                }
+                if (type == 'resize') {
+                    (function () {
+                        //隐藏菜单导航
+                        var winwidth = $win.width();
+                        if (winwidth >= 1200 || (winwidth >= 1200 && e.orientation == 'landscape')) {
+                            //隐藏已经存在的class
+                            //$header_btn.removeClass('header-btnactive');
+                            //$header_menu.removeClass('g-d-showi');
+                            isMobile = false;
+                        } else {
+                            isMobile = true;
+                        }
+
+
+                        //重新定位滚动条位置
+                        var i = 0,
+                            len = screen_pos.length,
+                            j = 0,
+                            pos = $win.scrollTop();
+                        for (i; i < len; i++) {
+                            var temptop = screen_pos[i]["node"].offset().top;
+                            screen_pos[i]["pos"] = temptop;
+
+                            var minpos = parseInt(pos - 150, 0),
+                                maxpos = parseInt(pos + 150, 0);
+                            if (temptop >= minpos && temptop <= maxpos) {
+                                $menu_item.eq(i).addClass('menu-active').siblings().removeClass('menu-active');
+                            }
+                        }
+
+
+                    }());
+
+                }
+            });
+        }());
+
 
     });
-
-
-    function getNoticeList(config) {
-        $.ajax({
-            url: "json/test.json",
-            dataType: 'JSON',
-            method: 'post'
-        }).done(function (resp) {
-            /*测试代码*/
-            var resp = testNoticeList();
-
-            var code = parseInt(resp.code, 10);
-            if (code !== 0) {
-                if (code === 999) {
-                    /*清空缓存*/
-                    config.$list.html('');
-                }
-                console.log(resp.message);
-                return false;
-            }
-            var result = resp.result;
-            if (!result) {
-                return false;
-            }
-            var list = result.list;
-            if (!list) {
-                return false;
-            }
-            var len = list.length,
-                i = 0,
-                str = '',
-                listitem;
-
-            for (i; i < len; i++) {
-                listitem = list[i];
-                if (i % 2 === 0) {
-                    str += '<li class="notice-lr"><span>' + listitem["dateTime"] + '</span><p>' + listitem["content"] + '</p></li>';
-                } else {
-                    str += '<li class="notice-ll"><span>' + listitem["dateTime"] + '</span><p>' + listitem["content"] + '</p></li>';
-                }
-            }
-
-            if(str!==''){
-                $(str).appendTo(config.$list);
-            }else{
-                config.$list.html('');
-            }
-        }).fail(function (resp) {
-            console.log(resp.message || 'error');
-            return false;
-        });
-    }
-
-
-    /*测试代码*/
-    function testNoticeList() {
-
-        var info=/(欢迎您光临|对不起，让您久等了|我能为您做什么？|对不起，我没有听明白，请您再讲一遍|很抱歉，您要办的事不符合规定，我们不能办理|不买没关系，欢迎您随便参观|这种商品暂时缺货，方便的话，请您留下姓名和电话，一有货马上通知您 ，好吗？|欢迎您以后常来){1}/,
-            res = {
-            message: 'ok',
-            code: 0,
-            result: Mock.mock({
-                'list|5-15': [{
-                    "id": /[0-9]{1,2}/,
-                    "dateTime": moment().format('YYYY-MM-DD'),
-                    "content":info
-                }]
-            })
-        };
-        return res;
-    }
-
-
 })(jQuery);
