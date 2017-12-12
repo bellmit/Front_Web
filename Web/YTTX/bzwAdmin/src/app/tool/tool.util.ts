51d9baed3fd21158c3afd5855b5054a885175c5a
yipin8715@sina.com <yipin8715@sina.com>
2017-12-12 18:37:06 +0800

    add or update

--- Web/YTTX/bzwAdmin/src/app/tool/tool.util.ts
+++ Web/YTTX/bzwAdmin/src/app/tool/tool.util.ts
@@ -1,6 +1,20 @@
 import {BaseConfig as bcof} from '../config/base.config';
 
-let base_config = bcof.getBaseConfig();
+let base_config = bcof.getBaseConfig(),
+  system_unique_key = base_config.unique_key || '',
+  tools = {
+    /*返回系统唯一标识符*/
+    getSystemUniqueKey() {
+      return system_unique_key;
+    },
+    /*判断是否支持本地存储*/
+    supportBox() {
+      let elem = document.getElementsByTagName('body')[0],
+        bs = window.getComputedStyle(elem, null).getPropertyValue("box-sizing") || document.defaultView.getComputedStyle(elem, null) || $(elem).css('boxSizing');
+      return bs && bs === 'border-box';
+    },
+  };
+
 export class ToolUtil {
 
 }
