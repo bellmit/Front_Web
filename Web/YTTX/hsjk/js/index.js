2a3b1ada1dc24dac6d265ddecb538719f9c51269
yipin8715@sina.com <yipin8715@sina.com>
2018-01-10 18:00:52 +0800

    华世健康

--- Web/YTTX/hsjk/js/index.js
+++ Web/YTTX/hsjk/js/index.js
@@ -9,21 +9,37 @@
             win_width = $win.width(),
             win_height = $win.height();
 
+
+        /*初始化*/
+
         /*dom节点缓存*/
-        var $hs_menuitem = $('#hs_menuitem');
+        var $hs_container_bg = $('#hs_container_bg'),
+            $hs_menuitem = $('#hs_menuitem');
 
 
         /*事件绑定*/
         $hs_menuitem.on('click', 'li', function () {
+            $hs_container_bg.css({
+                'background-image': 'url("images/show/' + ($(this).index() + 1) + '.jpg")'
+            });
+
             anime({
                 targets: '#hs_container_bg',
-                opacity: .5,
-                backgroundImage: 'url("images/show/' + ($(this).index() + 1) + '.jpg")',
-                easing: 'easeInOutQuad'
+                duration: 1000,
+                opacity: .8,
+                easing: 'easeInOutQuad',
+                complete: function () {
+                    anime({
+                        targets: '#hs_container_bg',
+                        duration: 200,
+                        opacity: .2,
+                        easing: 'easeInOutQuad'
+                    });
+                }
             })
-        })
+        }).find('>li:first-child').trigger('click');
 
-        /*初始化*/
+
 
     });
 
