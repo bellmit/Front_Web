674d8ea652c0a609b589272bd141140be8ae5929
yipin8715@sina.com <yipin8715@sina.com>
2018-01-10 17:14:09 +0800

    华世健康

--- Web/YTTX/hsjk/js/index.js
+++ Web/YTTX/hsjk/js/index.js
@@ -4,8 +4,26 @@
 (function ($) {
     'use strict';
     $(function () {
-        /*dom节点缓存*/
+        /*常用变量*/
+        var $win = $('body'),
+            win_width = $win.width(),
+            win_height = $win.height();
 
+        /*dom节点缓存*/
+        var $hs_menuitem = $('#hs_menuitem');
+
+
+        /*事件绑定*/
+        $hs_menuitem.on('click', 'li', function () {
+            anime({
+                targets: '#hs_container_bg',
+                opacity: .5,
+                backgroundImage: 'url("images/show/' + ($(this).index() + 1) + '.jpg")',
+                easing: 'easeInOutQuad'
+            })
+        })
+
+        /*初始化*/
 
     });
 
