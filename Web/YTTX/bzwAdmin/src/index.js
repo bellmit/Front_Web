d1214b5f256202863db1255bc512611efe415041
yipin8715@sina.com <yipin8715@sina.com>
2018-02-25 17:02:11 +0800

    webpack 测试

---
+++ Web/YTTX/bzwAdmin/src/index.js
@@ -0,0 +1,11 @@
+import $ from 'jquery';
+
+
+$(function () {
+    var $test=$('#test_support');
+
+    $test.on('click',function () {
+        var $this=$(this);
+        $this.html('您的浏览器版本太低，请升级您的浏览器'+Math.random().toString().slice(0,10)+'......');
+    });
+});
