0bfe8642d1380318bdf82418507c0d4dbf8f2b94
yipin8715@sina.com <yipin8715@sina.com>
2018-02-26 17:59:58 +0800

    webpack项目构建

--- Web/YTTX/bzwAdmin/webpack.config.js
+++ Web/YTTX/bzwAdmin/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const CleanWebpackPlugin=require('clean-webpack-plugin');
+const HtmlWebpackPlugin=require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -10,6 +12,18 @@ module.exports = {
         rules: [{
             test: /\.css$/,
             use: ['style-loader', 'css-loader']
+        }, {
+            test: /\.(woff|woff2|eot|ttf|otf)$/,
+            use: ['file-loader']
+        }, {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: ['file-loader']
         }]
-    }
+    },
+    plugins: [
+        /*new CleanWebpackPlugin(['dist']),*/
+        /*new HtmlWebpackPlugin({
+            title: 'webpack test'
+        })*/
+    ]
 };
